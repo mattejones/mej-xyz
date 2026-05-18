d24c9a3ffa162cd7cbc1846e35602f2b3fddd913
mattejones <mattejones@users.noreply.github.com>
2026-05-18 22:24:34 +0100

    ditch work on homepage

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   NAME: "Matt E. Jones",
   EMAIL: "me@mej.xyz",
   NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_WORKS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 0,
   NUM_PROJECTS_ON_HOMEPAGE: 5,
 };
 
