edb2f48b134f51940e5213bdd8117096dd945cfc
mattejones <mattejones@users.noreply.github.com>
2026-05-18 22:01:58 +0100

    improved chopper article, added new blog idea

--- src/consts.ts
+++ src/consts.ts
@@ -4,8 +4,8 @@ export const SITE: Site = {
   NAME: "Matt E. Jones",
   EMAIL: "me@mej.xyz",
   NUM_POSTS_ON_HOMEPAGE: 5,
-  NUM_WORKS_ON_HOMEPAGE: 0,
-  NUM_PROJECTS_ON_HOMEPAGE: 1,
+  NUM_WORKS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 5,
 };
 
 export const HOME: Metadata = {
